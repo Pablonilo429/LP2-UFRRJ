bae33ff5992ff21e88e5efe496615fc1221305c7
Pablo de Oliveira <pablo.oliveira.xavier@gmail.com>
2022-03-21 22:57:13 -0300

    Update Exer1.c
    Exercicio 1 completo

--- Exercicios lista 5/Exer1.c
+++ Exercicios lista 5/Exer1.c
@@ -5,10 +5,8 @@
 
 void maiuscula(char *str1){  //Transforma uma string em maiuscula
     int i;
-    for(i = 0; i < 30; i++){
-        if(str1[i] == '\0' && str1[i+1] == '\0'){
-            break;
-        }
+    int taman = strlen(str1);
+    for(i = 0; i < taman; i++){
         str1[i] = toupper(str1[i]);
     }
     printf("%s", str1);
@@ -18,19 +16,63 @@ void concatena(char *str1, char *str2){  //Concatena duas strings
     printf("%s",strncat(str1, str2, 60));   
 }
 
+int capitaliza(char *str){      //Contador de string e troca por maiusculo
+    int i;
+    int contador = 0;
+    int taman = strlen(str);    
+    for(i = 0; i < taman; i++){
+        if(i == 0){
+            str[i] = toupper(str[i]);
+        }
+        if(str[i] == ' '){
+            str[i+1] = toupper(str[i+1]);
+            contador++;
+        }
+    }
+    taman = taman - contador;
+    return taman;
+}
+
+int compara(char *str1, char *str2){
+    int comp = strcmp(str1, str2);
+    return comp;
+}
 
 
 int main(){
     char str1[30];
     char str2[30];
-    
+       
+    printf("Entre com duas strings:\n");
     gets(str1);
     gets(str2);
     
     maiuscula(&str1);
-    concatena(&str1, &str2);
+    printf("\n");
+    concatena(&str1, &str2); 
+    printf("\nNumero de caracteres: %d\n", capitaliza(&str2));
+    printf("%s\n", str2);
 
+    memset(str1, 0, 30);
+    memset(str2, 0, 30);
+
+    printf("Entre novamente com duas strings:\n");
+    gets(str1);
+    gets(str2);
+
+    switch(compara(&str2, &str1))
+    {
+        case -1: 
+            printf("A primeira string e menor que a segunda string\n");
+        break;
+        case 0: 
+            printf("A primeira string e igual a segunda string\n");
+        break;
+        case 1: 
+            printf("A primeira string e maior que a segunda string\n");
+        break; 
+    }
     
-
-
+    system("pause");
+    return 0;
 }
