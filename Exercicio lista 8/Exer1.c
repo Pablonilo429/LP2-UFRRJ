ae7aaf7055dc9d35b7e38d10641925d0ec0c2f67
Pablo de Oliveira <pablo.oliveira.xavier@gmail.com>
2022-04-14 23:09:47 -0300

    Create Exer1.c

---
+++ Exercicio lista 8/Exer1.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct 
+{
+    int x;
+    int y;
+    int z;
+}tpPonto;
+
+typedef struct{
+    tpPonto ponto;
+    char rotulo;
+}tpFigura;
+
+void preenche(tpFigura *var){
+    FILE *f;
+    int i = 0;
+    
+
+    f = fopen("desenho.dat", "wb");
+    if (f == NULL)
+        printf("\nErro, nao foi possivel abrir o arquivo!");
+    else {
+        for(i = 0;i < 2 ;i++){
+            fflush(stdin);
+            puts("Entre com o rotulo: ");
+            scanf("%c", &var[i].rotulo);
+            puts("Entre com o ponto X: ");
+            scanf("%d", &var[i].ponto.x);
+            puts("Entre com o ponto Y: ");
+            scanf("%d", &var[i].ponto.y);
+            puts("Entre com o ponto Z: ");
+            scanf("%d", &var[i].ponto.z);
+            fwrite(&var[i],sizeof(tpFigura),1,f);
+            
+        }
+    }
+    fclose(f);
+
+}
+
+void imprime(){
+    FILE *g;
+    tpFigura aux[2];
+    int i = 0;
+ 
+
+    g = fopen("desenho.dat", "rb");
+    if (g == NULL){
+        printf("\nErro, nao foi possivel abrir o arquivo!");
+    }
+    else{
+        
+        for(i = 0; i < 2; i++){
+            fseek(g,0, SEEK_SET);
+            fread(&aux[i], sizeof(tpFigura), 1, g);
+            
+
+            printf("Ponto %c: x = %d, y = %d, z = %d ", aux[i].rotulo, aux[i].ponto.x, aux[i].ponto.y, aux[i].ponto.z); 
+        }
+    }
+    
+    fclose(g);
+    
+}
+void apaga(tpFigura *var, char letra){
+    FILE *h;
+    int i;
+    long pos;
+    tpFigura aux;
+    
+ 
+
+    h = fopen("desenho.dat", "rb+");
+    if (h == NULL){
+        printf("\nErro, nao foi possivel abrir o arquivo!");
+    }
+    else{
+        for(i = 0; i < 10; i++){
+            if(var[i].rotulo == letra){
+               
+                var[i].rotulo = '#';
+                pos = ftell(h)-sizeof (tpFigura);
+                fseek (h, i*sizeof(tpFigura), SEEK_SET);
+                fwrite (&var[i].rotulo, sizeof(tpFigura),1,h);
+                    
+                
+            }
+        }
+        
+        
+    }   
+    fclose(h);
+}
+
+
+int main(){
+    tpFigura espacoR3[2];
+    char letra;
+    
+    
+    preenche(&espacoR3);
+    imprime();
+    puts("Entre com o retulo a ser excluido#: ");
+    fflush(stdin);
+    scanf("%c", &letra);
+    apaga(&espacoR3, letra);
+    imprime();
+
+
+    return 0;
+}
